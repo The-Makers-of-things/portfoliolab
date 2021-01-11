2fb4b871722860f535e0665333ccc1792e3b00d3
PanPip <panpipp@gmail.com>
2021-01-11 14:28:42 +0100

    Minor PortfolioLab updates

--- portfoliolab/bayesian/entropy_pooling.py
+++ portfoliolab/bayesian/entropy_pooling.py
@@ -1,7 +1,3 @@
-# Copyright 2019, Hudson and Thames Quantitative Research
-# All rights reserved
-# Read more: https://github.com/hudson-and-thames/mlfinlab/blob/master/LICENSE.txt
-
 #pylint: disable=missing-docstring
 import math
 import numpy as np
